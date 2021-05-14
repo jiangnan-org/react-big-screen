61c8a2ff12431cf1ce813927e80f085c056bf15c
Zhengyang550 <18151521911@163.com>
2021-05-14 23:25:28 +0800

    对接首页数据

---
+++ mock/addUsingPOST_3.mock.ts
@@ -0,0 +1,8 @@
+// @ts-ignore
+import { Request, Response } from 'express';
+
+export default {
+  'POST /inspection-record/add': (req: Request, res: Response) => {
+    res.status(200).send({});
+  },
+};
---
+++ mock/powerGenerationUsingGET.mock.ts
@@ -0,0 +1,8 @@
+// @ts-ignore
+import { Request, Response } from 'express';
+
+export default {
+  'GET /home/generation': (req: Request, res: Response) => {
+    res.status(200).send({});
+  },
+};
