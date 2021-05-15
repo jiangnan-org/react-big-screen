2c996d38b7e5a22cb56d3eeb260fc9b58ac6ca4c
Zhengyang550 <18151521911@163.com>
2021-05-16 00:11:53 +0800

    对接实时监控页面数据

--- src/pages/Monitor/DeviceMonitor/component/DeviceList/index.tsx
+++ src/pages/Monitor/DeviceMonitor/component/DeviceList/index.tsx
@@ -7,88 +7,107 @@ import React, { useState, useEffect, useRef } from 'react';
 import ProList from '@ant-design/pro-list';
 import Device from './Device';
 import styles from './index.less';
-import {DesktopOutlined} from '@ant-design/icons';
+import { DesktopOutlined } from '@ant-design/icons';
 import ResizeObserver from 'rc-resize-observer';
-import {Link} from 'umi';
+import { Link } from 'umi';
 import { getYuncangList } from '@/services/yuncang';
 import _ from 'lodash';
 import { useModel } from 'umi';
 import { ActionType } from '@ant-design/pro-table';
 
 export default () => {
+  // ref
+  const ref = React.useRef(null);
+
   // 默认页大小
-  const [pageSize,setPageSize] = useState(2);
+  const [pageSize, setPageSize] = useState(2);
 
   // 获取查询字段信息
-  const {fields} = useModel('monitor');
+  const { fields } = useModel('monitor');
 
   /** Table action 的引用，便于自定义触发 */
   const actionRef = useRef<ActionType>();
 
-  useEffect(()=>{
+  // 根据宽度调整
+  const handleOnResize = (offsetWidth) => {
+    if (offsetWidth > 1280) {
+      setPageSize(3);
+    } else {
+      setPageSize(2);
+    }
+  };
+
+  // 第一次加载成功
+  useEffect(() => {
+    // 根据宽度调整
+    if (ref.current) {
+      const width = ref.current ? ref.current.offsetWidth : 0;
+      handleOnResize(width);
+    }
+  }, [ref.current]);
+
+  useEffect(() => {
     actionRef.current?.reload();
-  },[fields])
+  }, [fields]);
 
   return (
-    <ResizeObserver
-      onResize={({offsetWidth}) => {
-        if(offsetWidth  > 1280){
-          setPageSize(3);
-        } else{
-          setPageSize(2);
-        }
-      }}
-    >
-    <ProList<API.Yuncang>
-      className={styles.deviceList}
-      pagination={{
-        pageSize,
-        showSizeChanger: false,
-        responsive:true
-      }}
-      actionRef={actionRef}
-      /*  一行几列数据 */
-      grid={{ gutter: 16, xs: 1, sm: 1, md: 2, lg: 2, xl: pageSize, xxl:3  }}
-      rowKey='id'
-      metas={{
-        title: {},
-        subTitle: {
-          dataIndex: 'name',
-          title: '云仓名称',
-        },
-        actions: {
-          render: (text,record) => [
-            <Link key={record.id} to={`/monitor/device-detail/${record.id}`}>查看详情</Link>
-          ]
-        },
-        avatar: {
-          render:()=>{
-            return <DesktopOutlined style={{color:'#0f93ea'}}/>
-          }
-        },
-        content: {},
-      }}
-      request={async (params: API.PageParams = {}) => {
-        // 追加查询参数
-        _.assign(params,{...fields});
+    <div ref={ref}>
+      <ResizeObserver
+        onResize={({ offsetWidth }) => {
+          handleOnResize(offsetWidth);
+        }}
+      >
+        <ProList<API.Yuncang>
+          className={styles.deviceList}
+          pagination={{
+            pageSize,
+            showSizeChanger: false,
+            responsive: true,
+          }}
+          actionRef={actionRef}
+          /*  一行几列数据 */
+          grid={{ gutter: 16, xs: 1, sm: 1, md: 2, lg: 2, xl: pageSize, xxl: pageSize }}
+          rowKey='id'
+          metas={{
+            title: {},
+            subTitle: {
+              dataIndex: 'name',
+              title: '云仓名称',
+            },
+            actions: {
+              render: (text, record) => [
+                <Link key={record.id} to={`/monitor/device-detail/${record.id}`}>查看详情</Link>,
+              ],
+            },
+            avatar: {
+              render: () => {
+                return <DesktopOutlined style={{ color: '#0f93ea' }} />;
+              },
+            },
+            content: {},
+          }}
+          request={async (params: API.PageParams = {}) => {
+            // 追加查询参数
+            _.assign(params, { ...fields });
 
-        // 这里需要返回一个 Promise,在返回之前你可以进行数据转化
-        const res: API.PageResponseMessage<API.Yuncang> = await getYuncangList(params);
+            // 这里需要返回一个 Promise,在返回之前你可以进行数据转化
+            const res: API.PageResponseMessage<API.Yuncang> = await getYuncangList(params);
 
-        // 功率曲线
-        _.forEach(res.data.records,record => {
-          _.assign(record,{content : (<Device yuncang={record}/>)})
-        })
+            // 功率曲线
+            _.forEach(res.data.records, record => {
+              _.assign(record, { content: (<Device yuncang={record} />) });
+            });
 
-        return {
-          data: res.data.records,
-          // success 请返回 true，不然 table 会停止解析数据，即使有数据
-          success: res.success,
-          // 不传会使用 data 的长度，如果是分页一定要传
-          total: res.data.total,
-        }
-      }}
-    />
-    </ResizeObserver>
+            return {
+              data: res.data.records,
+              // success 请返回 true，不然 table 会停止解析数据，即使有数据
+              success: res.success,
+              // 不传会使用 data 的长度，如果是分页一定要传
+              total: res.data.total,
+            };
+          }}
+        />
+      </ResizeObserver>
+    </div>
   );
 };
